039a123a0c2f75563a8b066f3b331d70c70a3407
“code-hopper” <ameykhondekar01@gmail.com>
2025-08-12 13:21:33 +0530

    leap year logics

--- lessons/5.javascript/5.statement/looping/loop.js
+++ lessons/5.javascript/5.statement/looping/loop.js
@@ -27,18 +27,120 @@
 
 // 0 to 100
 
-let start = 1
-let end = 10
+// let start = 1
+// let end = 10
 
-while (start <= end) {
-    if (start % 2 == 0) {
-        let number = start
-        let fact = 1
-        while (number != 0) {
-            fact = fact * number
-            number--
+// while (start <= end) {
+//     if (start % 2 == 0) {
+//         let number = start
+//         let fact = 1
+//         while (number != 0) {
+//             fact = fact * number
+//             number--
+//         }
+//         console.log(fact)
+//     }
+//     start++
+// }
+
+// repeating statement 
+
+// while (){
+// }
+
+// wap in js to print all even numbers from 0 to 1000
+
+// let number = 0
+
+// while (number < 1000) {
+//     if (number % 2 == 0) {
+//         console.log(number)
+//     }
+//     number++
+// }
+
+// leap year
+
+// let year = 1800
+
+// if(year % 100 == 0){
+//     if(year % 400 == 0){
+//         console.log(year + " is a leap year !")
+//     }else{
+//         console.log(year + " is not a leap year !")
+//     }
+// }else if(year % 4 == 0){
+//     console.log(year + " is a leap year !")
+// }else{
+//     console.log(year + " is not a leap year !")
+// }
+
+// if(((year % 100 == 0) && (year % 400 == 0)) || (year % 4 == 0)){
+//     console.log(year + " is a leap year !")
+// }else{
+//     console.log(year + " is not a leap year !")
+// }
+
+// if(((year % 100 != 0) && (year % 4 == 0)) || (year % 400 == 0)){
+//     console.log(year + " is a leap year !")
+// }else{
+//     console.log(year + " is not a leap year !")
+// }
+
+// let start = 1500
+// let org_start = start
+// let end = 2025
+
+// let count = 0
+
+// while (start <= end) {
+//     let year = start
+//     if (year % 100 == 0) {
+//         if (year % 400 == 0) {
+//             count++
+//             console.log(year)
+//         }
+//     } else if (year % 4 == 0) {
+//         count++
+//         console.log(year)
+//     }
+//     start++
+// }
+
+// console.log("their are total " + count + " leap year in range of " + org_start + " to " + end + " !")
+
+// let current_year = 2025
+// let next_leap_year = current_year + 1
+
+// while(!((next_leap_year % 100 != 0) && (next_leap_year % 4 == 0)) || (next_leap_year % 400 == 0)){
+//     next_leap_year++
+// }
+
+// console.log(next_leap_year)
+
+// (t && f) || (f)
+
+// f || f
+
+// f
+// t-> f
+
+let current_year = 2196
+
+let next_leap_year = current_year
+
+let willLoopRun = true
+
+while(willLoopRun){
+    next_leap_year++
+    let year = next_leap_year
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            willLoopRun = false
         }
-        console.log(fact)
+    } else if (year % 4 == 0) {
+        willLoopRun = false    
     }
-    start++
 }
+
+console.log(next_leap_year)
