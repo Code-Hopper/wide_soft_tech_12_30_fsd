ef03ec98cdb05b2d9662f60322cd65484f95d9d8
“code-hopper” <ameykhondekar01@gmail.com>
2025-08-21 13:44:51 +0530

    multi dimesional array and objects in js

--- lessons/5.javascript/8.Array/sorting.js
+++ lessons/5.javascript/8.Array/sorting.js
@@ -40,11 +40,46 @@ function bubbleSort(arr, order) {
     return `sort array is [${arr}] in ${steps} steps`
 }
 
-console.log(bubbleSort([3, 1, 2, 10, 7, 4, 9, 8, 5, 6],"A"))
-console.log(bubbleSort([3, 1, 2, 10, 7, 4, 9, 8, 5, 6],"D"))
+console.log(bubbleSort([3, 1, 2, 10, 7, 4, 9, 8, 5, 6], "A"))
+console.log(bubbleSort([3, 1, 2, 10, 7, 4, 9, 8, 5, 6], "D"))
 
-console.log(bubbleSort(["bhavesh", "amey", "sahil", "rohit"],"A"))
+console.log(bubbleSort(["bhavesh", "amey", "sahil", "rohit"], "A"))
 
-console.log(bubbleSort(["bhavesh", "amey", "sahil", "rohit"],"D"))
+console.log(bubbleSort(["bhavesh", "amey", "sahil", "rohit"], "D"))
 
 // selection sort
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] > arr[minIndex]) {
+                minIndex = j
+            }
+        }
+        let temp = arr[i]
+        arr[i] = arr[minIndex]
+        arr[minIndex] = temp
+    }
+    return arr
+}
+
+let numbers = [1, 5, 2, 3, 4]
+
+console.log("selection sort array : ", selectionSort(numbers))
+
+// 5(0) 2(1) 1(2) 3(3) 4(4)
+
+// 0 - 5, 2 - 1
+
+// swap
+
+// 1(0) 2(1) 5(2) 3(3) 4(4)
+
+// 2 - 5
+
+// 3 - 3
+
+// swap
+
+// 1 2 3 5 4
