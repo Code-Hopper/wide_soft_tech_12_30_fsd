96e815cd69cda30ab6293ea5120d05adeeed4dfb
“code-hopper” <ameykhondekar01@gmail.com>
2025-09-09 13:37:31 +0530

    task addittion and appending child task

--- assignments/assignment3/scripts/script.js
+++ assignments/assignment3/scripts/script.js
@@ -1,3 +1,5 @@
+let tasks = []
+
 let add_notes_button = document.querySelector("#add-notes-button")
 
 let targetElement = document.querySelector(".add-notes-pop-up")
@@ -26,8 +28,48 @@ addNotesFormContainer.addEventListener("mouseleave", () => {
     document.getElementById("formSubmitButton").click()
 })
 
+let taskObject = {
+    title: "",
+    description: "",
+    timeStamp: ""
+}
+
 taskForm.addEventListener('submit', (event) => {
     event.preventDefault()
-    // alert('tring to add a new task form submit !')
-    console.log(event.target)
+    try {
+
+        if (!event.target["title"].value || !event.target["description"].value) {
+            throw ("empty fields !")
+        }
+
+        taskObject.title = event.target["title"].value
+        taskObject.description = event.target["description"].value
+        taskObject.timeStamp = `T: ${new Date().toLocaleTimeString()} D: ${new Date().toLocaleDateString()}`
+        console.log(taskObject)
+        tasks.push(taskObject)
+        event.target["title"].value = ""
+        event.target["description"].value = ""
+        closeButton.click()
+        displayTask()
+    } catch (err) {
+        console.log("please added task data before submitting ! : ", err)
+    }
 })
+
+function displayTask() {
+
+    document.querySelector('.tasks-container').innerHTML = ""
+
+    tasks.forEach((task) => {
+        let singleTask = document.createElement("div")
+        singleTask.classList.value = "task border p-4"
+        singleTask.innerHTML = `
+                            <h4 class="title">${task.title}</h4>
+                            <p class="description">
+                                ${task.description}
+                            </p>
+                            <span class="timeStamp">${task.timeStamp}</span>
+    `
+        document.querySelector('.tasks-container').appendChild(singleTask)
+    })
+}
